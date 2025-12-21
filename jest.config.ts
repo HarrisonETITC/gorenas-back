8cefdab8aad16d6c6ed9f287f8f2016292ec2ac4
HarrisonETITC <hdramirezb@itc.edu.co>
2025-12-21 18:07:01 +0000

    Ajustes para test en pipeline

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import { compilerOptions } from "./tsconfig.json"
 import { pathsToModuleNameMapper } from 'ts-jest';
 
 export default {
-  maxWorkers: 4,
+  maxWorkers: 3,
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: 'src',
@@ -19,8 +19,31 @@ export default {
   ],
   coverageDirectory: "../coverage",
   coveragePathIgnorePatterns: [
-  "/node_modules/",
-  "/dist/",
-  "/cdk/"
-]
+    "/node_modules/",
+    "/dist/",
+    "/cdk/"
+  ],
+  // Reportes de cobertura en múltiples formatos
+  coverageReporters: [
+    'text',           // Muestra en consola
+    'text-summary',   // Resumen en consola
+    'lcov',           // Para herramientas como SonarQube
+    'cobertura',      // Para Azure DevOps (coverage)
+    'html',           // Reporte HTML local
+    'json'            // JSON para procesamiento adicional
+  ],
+  // Reportes de resultados de tests
+  reporters: [
+    'default',        // Reporter estándar de Jest
+    ['jest-junit', {
+      outputDirectory: './coverage',
+      outputName: 'junit.xml',
+      ancestorSeparator: ' › ',
+      uniqueOutputName: false,
+      suiteNameTemplate: '{filepath}',
+      classNameTemplate: '{classname}',
+      titleTemplate: '{title}',
+      addFileAttribute: true
+    }]
+  ]
 }
