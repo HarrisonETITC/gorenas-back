a781ffd68c88b6d3af42a99954ce9957cccb05f0
HarrisonETITC <127680516+HarrisonETITC@users.noreply.github.com>
2026-01-04 01:26:39 -0500

    Script de python para backup de data

---
+++ scripts/backup-data.py
@@ -0,0 +1,65 @@
+import boto3
+import subprocess
+from datetime import datetime
+import os
+import sys
+import mysql.connector
+from mysql.connector import Error
+
+DB_HOST = os.environ['DB_HOST']
+DB_PORT = int(os.environ['DB_PORT'])
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_NAME = os.environ['DB_NAME']
+
+BACKUP_DIR = "./"
+
+def verify_mysql_connection():
+    try:
+        connection = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME,
+            port=DB_PORT,
+            connection_timeout=6
+        )
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT 1")
+        cursor.fetchone()
+
+        cursor.close()
+        connection.close()
+
+        print("Connection to MySQL successful")
+        return True
+
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+        sys.exit(1)
+
+def create_backup_file():
+    verify_mysql_connection()
+    # timestamp = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
+    # backup_file = os.path.join(BACKUP_DIR, f"{DB_NAME}_data_{timestamp}.sql")
+
+    # command = [
+    #     "mysqldump",
+    #     "--host", DB_HOST,
+    #     "--user", DB_USER,
+    #     f"--password={DB_PASSWORD}",
+    #     "--no-create-info",
+    #     "--skip-triggers",
+    #     "--single-transaction",
+    #     "--quick",
+    #     DB_NAME
+    # ]
+
+    # with open(backup_file, "w") as f:
+    #     subprocess.run(command, stdout=f, stderr=subprocess.PIPE, check=True)
+
+    # print(f"[INFO] Backup file created at {backup_file}")
+
+if __name__ == "__main__":
+    create_backup_file()
