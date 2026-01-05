21c05b0f34c0f10dde1f431d39a15b16a32cdf62
Harrison237 <harrisonramirez237@hotmail.com>
2026-01-04 23:07:58 -0500

    Subida de archivo backup en bucket s3

--- scripts/backup-data.py
+++ scripts/backup-data.py
@@ -1,10 +1,15 @@
 import boto3
+from botocore.exceptions import ClientError
 import subprocess
-from datetime import datetime
+from datetime import datetime, UTC
 import os
 import sys
 import mysql.connector
 from mysql.connector import Error
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 DB_HOST = os.environ['DB_HOST']
 DB_PORT = int(os.environ['DB_PORT'])
@@ -13,6 +18,9 @@ DB_PASSWORD = os.environ['DB_PASS']
 DB_NAME = os.environ['DB_NAME']
 
 BACKUP_DIR = "./"
+
+S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', 'my-backup-bucket')
+s3 = boto3.client('s3', region_name='us-east-1')
 
 def verify_mysql_connection():
     try:
@@ -32,17 +40,19 @@ def verify_mysql_connection():
         cursor.close()
         connection.close()
 
-        print("Connection to MySQL successful")
+        logger.info("Connection to MySQL successful")
         return True
 
     except Error as e:
-        print("Error while connecting to MySQL", e)
+        logger.error("Error while connecting to MySQL", e)
         sys.exit(1)
 
 def create_backup_file():
     verify_mysql_connection()
-    timestamp = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
+    timestamp = datetime.now(UTC).strftime("%Y%m%d_%H%M%S")
     backup_file = os.path.join(BACKUP_DIR, f"{DB_NAME}_data_{timestamp}.sql")
+
+    logging.info(f"Creating backup of database {DB_NAME} in file: {backup_file}")
 
     command = [
         "mysqldump",
@@ -64,11 +74,24 @@ def create_backup_file():
     with open(backup_file, "w") as f:
         subprocess.run(command, stdout=f, stderr=subprocess.PIPE, check=True)
 
-    print(f"[INFO] Backup file created at {backup_file}")
+    logger.info(f"Backup file created at {backup_file}")
+    upload_to_s3(backup_file)
 
-    with open(backup_file, "r") as f:
-        content = f.read()
-        print(content[:500])  # Print first 500 characters of the backup file
+def upload_to_s3(file_path):
+    file_name = os.path.basename(file_path)
+    s3_file_key = f"backups/{file_name}"
+
+    try:
+        s3.upload_file(
+            Filename=file_path,
+            Bucket=S3_BUCKET_NAME,
+            Key=s3_file_key,
+            ExtraArgs=["ServerSideEncryption=AES256"]
+        )
+        logger.info(f"Uploaded {file_name} to {s3_file_key} in bucket {S3_BUCKET_NAME}")
+    except ClientError as e:
+        logger.error(f"Failed to upload {file_name} to S3: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     create_backup_file()
